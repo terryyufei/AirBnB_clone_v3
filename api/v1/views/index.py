87a93c6854ab7a2ab5ea20d5b48c4079cc9f7a3c
Terry Wambui <shikuwambui@gmail.com>
2023-09-29 23:15:31 +0300

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,24 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def get_status():
     """Checks and returns status"""
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the no of each object by type"""
+    objects = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+            }
+    return jsonify(objects)
