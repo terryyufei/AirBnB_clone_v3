2af6ac7c6ba879daf5e591c8666f846f078b57b6
Terry Wambui <shikuwambui@gmail.com>
2023-09-29 22:39:16 +0300

    revert changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,7 +22,7 @@ def teardown(error):
 
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
-            threaded=True) 
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
+    
